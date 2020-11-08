df5592ac9fbb0c27c859fd5fffb614c8371914e6
Ryan Mehri <ryan.mehri1@gmail.com>
2020-11-07 20:53:22 -0700

    Add basic progress bar without correct percent

--- src/UI.hs
+++ src/UI.hs
@@ -5,15 +5,32 @@ module UI where
 import Brick
 import qualified Brick.AttrMap as A
 import qualified Brick.Main as M
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Widgets.Center as C
 import Brick.Widgets.Edit
 import qualified Brick.Widgets.Edit as E
+import qualified Brick.Widgets.ProgressBar as P
+import qualified Data.Function as F
 import qualified Data.Text as T
 import qualified Graphics.Vty as V
 import Lens.Micro
 import Thock
 
 draw :: Game -> [Widget Name]
-draw g = [txt (g ^. prompt) <=> renderEditor (txt . T.unlines) True (g ^. input)]
+draw g = [progressWidget <=> promptWidget <=> inputWidget]
+  where
+    progressWidget = drawProgressBar g
+    promptWidget = addBorder "prompt" (C.center $ txt (g ^. prompt))
+    inputWidget = addBorder "input" (renderEditor (txt . T.unlines) True (g ^. input))
+
+drawProgressBar :: Game -> Widget Name
+drawProgressBar g = addBorder "progress" (P.progressBar (Just (show $ progressPercent * 100)) progressPercent)
+  where
+    progressPercent = ((/) `F.on` fromIntegral) (T.length (T.unwords $ E.getEditContents $ g ^. input)) (T.length (g ^. prompt))
+
+addBorder :: T.Text -> Widget Name -> Widget Name
+addBorder t = withBorderStyle BS.unicodeRounded . B.borderWithLabel (txt t)
 
 handleKey :: Game -> BrickEvent Name e -> EventM Name (Next Game)
 handleKey g (VtyEvent ev) =
@@ -26,7 +43,8 @@ theMap :: A.AttrMap
 theMap =
   A.attrMap
     V.defAttr
-    [ (E.editFocusedAttr, V.black `on` V.yellow)
+    [ (P.progressCompleteAttr, V.black `on` V.white),
+      (P.progressIncompleteAttr, V.white `on` V.black)
     ]
 
 theApp :: M.App Game e Name
