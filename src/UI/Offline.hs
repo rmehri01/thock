443a7462b7b77d546b7ea461ad865fda0fd8d232
413x <dedifferentiator@protonmail.ch>
2021-01-11 18:59:40 +0200

    fmt: UI/Offline.hs

--- src/UI/Offline.hs
+++ src/UI/Offline.hs
@@ -71,22 +71,22 @@ import UI.Common
 localApp :: M.App Game () ResourceName
 localApp =
   M.App
-    { M.appDraw = drawGame,
-      M.appChooseCursor = showFirstCursor,
-      M.appHandleEvent = handleKeyGame,
-      M.appStartEvent = return,
-      M.appAttrMap = const attributeMap
+    { M.appDraw         = drawGame
+    , M.appChooseCursor = showFirstCursor
+    , M.appHandleEvent  = handleKeyGame
+    , M.appStartEvent   = return
+    , M.appAttrMap      = const attributeMap
     }
 
 -- | Draws the given 'Game' based on its current state
 drawGame :: Game -> [Widget ResourceName]
 drawGame g = case g of
-  MainMenu l -> drawList l
-  OnlineSelect l -> drawList l
-  CreateRoomMenu form -> drawForm form
-  JoinRoomMenu form -> drawForm form
-  Practice gs -> drawPractice gs
-  ErrorOverlay prev t -> drawError prev t
+  MainMenu       l      -> drawList     l
+  OnlineSelect   l      -> drawList     l
+  CreateRoomMenu form   -> drawForm     form
+  JoinRoomMenu   form   -> drawForm     form
+  Practice       gs     -> drawPractice gs
+  ErrorOverlay   prev t -> drawError    prev t
 
 -- | Renders a list widget under the logo
 drawList :: MenuList -> [Widget ResourceName]
@@ -98,63 +98,70 @@ drawList l = [drawMenu listWidget]
 drawForm :: RoomForm a -> [Widget ResourceName]
 drawForm form = [drawMenu formWidget]
   where
-    formWidget = vLimit 6 $ hLimitPercent 80 (renderForm form)
+    formWidget = vLimit 6 $ hLimitPercent 80 $ renderForm form
 
 -- | Draws the title over another widget w
 drawMenu :: Widget ResourceName -> Widget ResourceName
-drawMenu w = addBorder "" (C.center titleWidget <=> padBottom Max (C.hCenter w))
+drawMenu w = addBorder "" $ C.center titleWidget <=> padBottom Max (C.hCenter w)
 
 -- | Draw a practice game based on its 'GameState'
 drawPractice :: GameState -> [Widget ResourceName]
 drawPractice g =
-  [ drawFinished g "Back: Esc | Retry: ^r | Next: ^n",
-    drawProgressBarGameState g <=> drawPrompt g <=> drawInput g
+  [ drawFinished             g "Back: Esc | Retry: ^r | Next: ^n"
+  , drawProgressBarGameState g <=> drawPrompt g <=> drawInput g
   ]
 
 -- | Draws an error popup over the 'Game'
 drawError :: Game -> T.Text -> [Widget ResourceName]
 drawError g t = errorPopup : drawGame g
-  where
-    errorPopup = C.centerLayer . hLimitPercent 80 . addBorder "error" $ withAttr redAttr (txtWrap t)
+  where errorPopup = C.centerLayer . hLimitPercent 80 . addBorder "error"
+                     $ withAttr redAttr $ txtWrap t
 
 -- | Handles events based on the current state of the 'Game'
 handleKeyGame :: Game -> BrickEvent ResourceName () -> EventM ResourceName (Next Game)
 handleKeyGame gs ev = case gs of
-  MainMenu l -> handleKeyMainMenu l ev
-  OnlineSelect l -> handleKeyOnlineSelect l ev
-  CreateRoomMenu form -> handleKeyForm CreateRoomMenu (\u -> generateRoomId >>= runClient True . RoomFormData u) (^. value) form ev
-  JoinRoomMenu form -> handleKeyForm JoinRoomMenu (runClient False) (^. (username . value)) form ev
-  Practice g -> handleKeyPractice g ev
-  ErrorOverlay prev _ -> M.continue prev -- after receiving any event, remove the error overlay
+  MainMenu       l      -> handleKeyMainMenu     l ev
+  OnlineSelect   l      -> handleKeyOnlineSelect l ev
+  CreateRoomMenu form   -> handleKeyForm CreateRoomMenu crmf cu form ev
+  JoinRoomMenu   form   -> handleKeyForm JoinRoomMenu   jrmf ju form ev
+  Practice       g      -> handleKeyPractice     g ev
+  ErrorOverlay   prev _ -> M.continue            prev -- after receiving any event, remove the error overlay
+  where crmf u = runClient True . RoomFormData u =<< generateRoomId
+        jrmf   = runClient False
+
+        cu = (^. value)
+        ju = (^. (username . value))
 
 -- | Handles key events for navigating the 'MainMenu'
 handleKeyMainMenu :: MenuList -> BrickEvent ResourceName e -> EventM ResourceName (Next Game)
 handleKeyMainMenu l (VtyEvent e) = case e of
-  V.EvKey V.KEsc [] -> M.halt (MainMenu l)
+  V.EvKey V.KEsc   [] -> M.halt $ MainMenu l
   V.EvKey V.KEnter []
-    | Just i <- L.listSelected l ->
-      if i == 0
-        then liftIO generateQuote >>= M.continue . startPracticeGame
-        else M.continue onlineSelectState
-  ev -> L.handleListEvent ev l >>= M.continue . MainMenu
-handleKeyMainMenu l _ = M.continue (MainMenu l)
+    | Just i <- L.listSelected l -> pracOrOnline i
+  ev -> M.continue . MainMenu =<< L.handleListEvent ev l
+
+  where pracOrOnline i =
+          if i == 0
+          then M.continue . startPracticeGame =<< liftIO generateQuote
+          else M.continue   onlineSelectState
+handleKeyMainMenu l _ = M.continue $ MainMenu l
 
 -- | Handles key events for creating or joining an online room
 handleKeyOnlineSelect :: MenuList -> BrickEvent ResourceName e -> EventM ResourceName (Next Game)
 handleKeyOnlineSelect l (VtyEvent e) = case e of
-  V.EvKey V.KEsc [] -> M.continue initialGame
+  V.EvKey V.KEsc   []            -> M.continue initialGame
   V.EvKey V.KEnter []
-    | Just i <- L.listSelected l ->
-      M.continue
-        ( if i == 0
-            then CreateRoomMenu (makeCreateRoomForm emptyUsername)
-            else JoinRoomMenu (makeJoinRoomForm emptyRoomFormData)
-        )
-    where
-      emptyUsername = Username ""
-      emptyRoomFormData = RoomFormData emptyUsername ""
-  ev -> L.handleListEvent ev l >>= M.continue . OnlineSelect
-handleKeyOnlineSelect l _ = M.continue (OnlineSelect l)
+    | Just i <- L.listSelected l -> M.continue . createOrJoin $ i
+  ev                             -> M.continue . OnlineSelect =<< L.handleListEvent ev l
+
+  where createOrJoin i =
+          if i == 0
+          then CreateRoomMenu $ makeCreateRoomForm emptyUsername
+          else JoinRoomMenu   $ makeJoinRoomForm   emptyRoomFormData
+
+        emptyUsername     = Username ""
+        emptyRoomFormData = RoomFormData emptyUsername ""
+handleKeyOnlineSelect l _ = M.continue $ OnlineSelect l
 
 -- | Handles a key event for a form and its validation
 handleKeyForm ::
@@ -168,15 +175,19 @@ handleKeyForm ::
   BrickEvent ResourceName () ->
   EventM ResourceName (Next Game)
 handleKeyForm ctr onEnter getUser form ev@(VtyEvent e) = case e of
-  V.EvKey V.KEsc [] -> M.continue initialGame
-  V.EvKey V.KEnter [] ->
-    if not . T.null $ getUser (formState form)
-      then
-        M.suspendAndResume
-          (maybe initialGame (ErrorOverlay (ctr form)) <$> onEnter (formState form))
-      else M.continue (ErrorOverlay (ctr form) "username cannot be empty")
-  _ -> handleFormEvent ev form >>= M.continue . ctr
-handleKeyForm ctr _ _ form _ = M.continue (ctr form)
+  V.EvKey V.KEsc   [] -> M.continue initialGame
+  V.EvKey V.KEnter [] -> enterH
+  _                   -> M.continue . ctr =<< handleFormEvent ev form
+
+  where isUserNonEmpty = not . T.null $ getUser $ formState form
+        game   = maybe initialGame eo <$> onEnter (formState form)
+        enterH = if isUserNonEmpty
+                 then M.suspendAndResume game
+                 else M.continue         eos
+
+        eo   = ErrorOverlay $ ctr form
+        eos  = ErrorOverlay  (ctr form) "username cannot be empty"
+handleKeyForm ctr _ _ form _ = M.continue $ ctr form
 
 -- | Construct a 'RoomForm' with the given 'Username' as the initial state
 makeCreateRoomForm :: Username -> RoomForm Username
@@ -193,8 +204,8 @@ makeJoinRoomForm =
   newForm
     [ formLabel "Username"
         @@= addBorder ""
-        @@= editTextField (username . value) UsernameField (Just 1),
-      formLabel "Room ID"
+        @@= editTextField (username . value) UsernameField (Just 1)
+    , formLabel "Room ID"
         @@= addBorder ""
         @@= editTextField roomId RoomIdField (Just 1)
     ]
@@ -203,24 +214,25 @@ makeJoinRoomForm =
 formLabel :: T.Text -> Widget n -> Widget n
 formLabel t w = C.vCenter label <+> C.vCenter w
   where
-    label = vLimit 1 (hLimit 15 $ withAttr secondaryAttr $ txt t <+> fill ' ')
+    label = vLimit 1 $ hLimit 15 $ withAttr secondaryAttr $ txt t <+> fill ' '
 
 -- | Handles key events for updating the 'GameState' in a 'Practice' game
 handleKeyPractice :: GameState -> BrickEvent ResourceName e -> EventM ResourceName (Next Game)
 handleKeyPractice g (VtyEvent ev) =
   case ev of
-    V.EvKey V.KEsc [] -> M.continue initialGame
-    V.EvKey (V.KChar 'r') [V.MCtrl] -> M.continue (startPracticeGame (g ^. quote))
-    V.EvKey (V.KChar 'n') [V.MCtrl] -> liftIO generateQuote >>= M.continue . startPracticeGame
-    V.EvKey (V.KChar _) [] -> nextState $ numStrokes g
-    _ -> nextState g
+    V.EvKey  V.KEsc       []        -> M.continue   initialGame
+    V.EvKey (V.KChar 'r') [V.MCtrl] -> M.continue . startPracticeGame $ g ^. quote
+    V.EvKey (V.KChar 'n') [V.MCtrl] -> M.continue . startPracticeGame =<< liftIO generateQuote
+    V.EvKey (V.KChar _  ) []        -> nextState  $ numStrokes g
+    _                               -> nextState g
+
   where
     nextState g' =
       if isDone g'
-        then M.continue (Practice g')
-        else updateGameState g' ev >>= M.continue . Practice
+        then M.continue . Practice $ g'
+        else M.continue . Practice =<< updateGameState g' ev
     numStrokes g' =
       if isDone g'
         then g'
         else g' & strokes +~ 1
-handleKeyPractice g _ = M.continue (Practice g)
+handleKeyPractice g _ = M.continue $ Practice g
